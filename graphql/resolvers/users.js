31d763c43a29a418621c9d186017e74ea70712f8
Riya Gupta <riyag2803@gmail.com>
2022-10-11 00:31:32 -0700

    register vaidations

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
+const { validateRegisterInput } = require("../../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
@@ -11,8 +12,18 @@ module.exports = {
       _,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // TODO: Validate user data
-      // TODO: make sure user doesn't already exists
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      // make sure user doesn't already exists
       const user = await User.findOne({ username });
       if (user) {
         throw new UserInputError("Username is taken", {
