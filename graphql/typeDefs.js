7fae7041e517890bc0ca5650c675cbe89a6b964b
Riya Gupta <riyag2803@gmail.com>
2022-10-11 06:29:28 -0700

    Feature/likes and comments (#10)
    * comment and like features added
    
    * like and comment count; pubsub

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,21 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  type Comment {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+  type Like {
+    id: ID!
+    username: String!
+    createdAt: String!
   }
   type User {
     id: ID!
@@ -33,5 +48,11 @@ module.exports = gql`
     login(loginInput: LoginInput): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription {
+    newPost: Post!
   }
 `;
