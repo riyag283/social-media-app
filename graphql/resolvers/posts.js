8013db029ddb1fd96d16d751fbd2374c9f9c6cbf
Riya Gupta <riyag2803@gmail.com>
2022-10-11 05:53:35 -0700

    comment and like features added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,5 @@
-const { AuthenticationError } = require("apollo-server");
-const post = require("../../models/Post");
+const { AuthenticationError, UserInputError } = require("apollo-server");
+
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -46,7 +46,9 @@ module.exports = {
       const user = checkAuth(context);
       try {
         const post = await Post.findById(postId);
-        if (user.username === post.username) {
+        if (!post) {
+          throw new UserInputError("Post not found");
+        } else if (user.username === post.username) {
           await post.delete();
           return "Post deleted successfully";
         } else {
@@ -55,6 +57,24 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes?.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,19 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+  }
+  type Comment {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+  type Like {
+    id: ID!
+    username: String!
+    createdAt: String!
   }
   type User {
     id: ID!
@@ -33,5 +46,8 @@ module.exports = gql`
     login(loginInput: LoginInput): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
